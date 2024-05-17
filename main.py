de57b63760d081fc34de02f3b25c46acbb653e13
zecoy <96514541+zecoy@users.noreply.github.com>
2024-05-17 10:01:42 +0800

    Update main.py

--- main.py
+++ main.py
@@ -129,7 +129,7 @@ def get_args_parser():
 
     # * dataset parameters
     parser.add_argument('--dataset_file', default='coco')
-    parser.add_argument('--coco_path', default='./data/coco', type=str)
+    parser.add_argument('--coco_path', default='/kaggle/input/coco-2017-dataset/coco2017', type=str)
     parser.add_argument('--coco_panoptic_path', type=str)
     parser.add_argument('--remove_difficult', action='store_true')
 
